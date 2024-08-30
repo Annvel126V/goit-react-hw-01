be3be8ee401827fabe58ce65c156252b0685bc07
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-08-30 13:34:49 +0200

    add profile

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,22 +1,5 @@
-import FriendListItem from '.FriendListItem/FriendListItem';
-import css from './FriendListItem.module.css';
-
-const FriendList = ({ friends }) => {
-  return (
-    <ul>
-      {friends.map(friend => {
-        return (
-          <li key={friend.id}>
-            <FriendListItem
-              avatar={friend.avatar}
-              name={friend.name}
-              isOnline={friend.isOnline}
-            />
-          </li>
-        );
-      })}
-    </ul>
-  );
+const FriendListItem = () => {
+  return <div>FriendListItem</div>;
 };
 
-export default FriendList;
+export default FriendListItem;
